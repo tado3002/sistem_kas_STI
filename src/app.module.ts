aee831a8d4d4f36a244febb178546a6026b72f21
Muhammad Murtadlo <muh.murtadlo23@gmail.com>
2024-12-11 16:47:55 +0700

    memperbarui semua module

--- src/app.module.ts
+++ src/app.module.ts
@@ -3,11 +3,14 @@ import { AppController } from './app.controller';
 import { AppService } from './app.service';
 import { PrismaService } from './prisma/prisma.service';
 import { DosensModule } from './dosens/dosens.module';
+import { AuthModule } from './auth/auth.module';
+import { UsersModule } from './users/users.module';
+import { MahasiswaModule } from './mahasiswa/mahasiswa.module';
 
 @Module({
   controllers: [AppController],
   providers: [AppService, PrismaService],
   exports: [PrismaService],
-  imports: [DosensModule],
+  imports: [DosensModule, AuthModule, UsersModule, MahasiswaModule],
 })
 export class AppModule {}
